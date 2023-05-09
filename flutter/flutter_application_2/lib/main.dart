f62a35289e070d182ecd56dba0e234378c794c72
hemthath313 <hemthath313@gmail.com>
2023-05-09 12:20:38 +0530

    completed scaffold

---
+++ flutter/flutter_application_2/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Mineapp());
+}
+
+class Mineapp extends StatelessWidget {
+  // const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+              title: const Text("welcome to our page"),
+              leading: IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                },
+              )
+              ),
+              body: const Center(
+                child:Text("welcome you all"
+                ),
+              ),
+              ),
+    );
+  }
+}
